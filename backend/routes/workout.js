df33e53154e629078650861c00382edbd09e5e7d
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-02-24 00:42:24 +0600

    created model and pushed data in the mongoDB

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,31 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Workout = require("../models/workoutModel");
+
 //All workouts
-router.get('/',(req,res)=>{
-    res.json({message: 'get workouts'});
-})
+router.get("/", (req, res) => {
+  res.json({ message: "get workouts" });
+});
 
 //Gets a single workout by id
-router.get('/:id',(req,res)=>{
-    res.json({message: 'get workout by id'});
-})
-
+router.get("/:id", (req, res) => {
+  res.json({ message: "get workout by id" });
+});
 
 //Create a new workout
 
-router.post('/',(req,res)=>{
-    res.json({message: 'created workout'});
-})
+router.post("/", async (req, res) => {
+  const { title, reps, sets } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, sets });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ error: err});
+  }
+});
 
 //Delete a workout by id
-router.delete('/:id',(req,res)=>{
-    res.json({message: 'deleted workout'});
-})
+router.delete("/:id", (req, res) => {
+  res.json({ message: "deleted workout" });
+});
 
 //Update a workout by id
-router.patch('/:id',(req,res)=>{
-    res.json({message: 'Workout Updated'});
-})
+router.patch("/:id", (req, res) => {
+  res.json({ message: "Workout Updated" });
+});
 
 module.exports = router;
