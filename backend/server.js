410327ab6eeda743ddd7e8768247dde3a11fac74
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-02-23 20:54:24 +0600

    Add MongoDB and Mongoose dependencies, update server.js to connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const workoutRoutes = require('./routes/workout');
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workout");
 // Create an express app
 const app = express();
 
 //middleware
 app.use(express.json());
 
-app.use((req,res, next)=>{
-    console.log(req.path, req.method);
-    next();
-})
-
-//routes
-app.use('/api/workouts', workoutRoutes);
-
-
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Server is listening on port', process.env.PORT);
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Server is listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
