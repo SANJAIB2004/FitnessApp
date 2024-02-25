4ae837aa2f336dd52863d17028583c209e49e0be
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-02-25 17:17:10 +0600

    Update styles and fetch workouts in Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:3000/api/workouts");
+      const data = await response.json();
+      if (response.ok) {
+        setWorkouts(data);
+      } else {
+        console.log(data.message);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
