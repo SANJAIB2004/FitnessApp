a91c11d80ca8162d6af5c6a2381a4856e6a234e1
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-02-25 15:26:42 +0600

    Navbar added and start of some features

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/" className="logo">
+          <h1>Workout Planner</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
